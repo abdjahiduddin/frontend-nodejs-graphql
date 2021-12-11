927ab6f0b3cdb3b5e58003d4afc80eb3fedd3e66
abd.jahiduddin@gmail.com <abd.jahiduddin@gmail.com>
2021-12-11 15:34:27 +0800

    Updates

---
+++ precache-manifest.ac5298bfdf382b600cc66098bdb333ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f667eaf72df00f8020c4",
+    "url": "/frontend-nodejs-graphql/static/css/main.ad48569f.chunk.css"
+  },
+  {
+    "revision": "f667eaf72df00f8020c4",
+    "url": "/frontend-nodejs-graphql/static/js/main.f667eaf7.chunk.js"
+  },
+  {
+    "revision": "e124f9e421c368f92402",
+    "url": "/frontend-nodejs-graphql/static/js/1.e124f9e4.chunk.js"
+  },
+  {
+    "revision": "398e90b4dc7ec069910d",
+    "url": "/frontend-nodejs-graphql/static/js/runtime~main.398e90b4.js"
+  },
+  {
+    "revision": "8dcfbc5ff335347382da0b75b80378a4",
+    "url": "/frontend-nodejs-graphql/index.html"
+  }
+];
